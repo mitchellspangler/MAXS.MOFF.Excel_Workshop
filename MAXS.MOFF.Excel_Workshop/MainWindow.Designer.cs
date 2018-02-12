857475fda40adefeca51d60fba98471d2b40104c
Mitchell Spangler <spamy009@mymail.unisa.edu.au>
2018-02-12 20:20:09 +1030

    Init Commit
    Starting commit, wrote ExcelHandler which will be an inner namespace
    insertion into [ref].Excel for a GUI based interface utilize as an
    interaction library.

---
+++ MAXS.MOFF.Excel_Workshop/MainWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MAXS.MOFF.Excel_Workshop
+{
+    using Microsoft.Vbe.Interop;
+
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgThread = new System.ComponentModel.BackgroundWorker();
+            this.TestBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TestBtn
+            // 
+            this.TestBtn.Location = new System.Drawing.Point(12, 12);
+            this.TestBtn.Name = "TestBtn";
+            this.TestBtn.Size = new System.Drawing.Size(75, 23);
+            this.TestBtn.TabIndex = 0;
+            this.TestBtn.Text = "Test Method ";
+            this.TestBtn.UseVisualStyleBackColor = true;
+            this.TestBtn.Click += new System.EventHandler(this.TestBtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.TestBtn);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgThread;
+        private System.Windows.Forms.Button TestBtn;
+    }
+}
+
---
+++ MAXS.MOFF.Excel_Workshop/MainWindow.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Vbe.Interop;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+
+namespace MAXS.MOFF.Excel_Workshop
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TestBtn_Click(object sender, EventArgs e)
+        {
+            //init excel doc
+            ExcelHandler myHandler = new ExcelHandler();
+        }
+    }
+}
+
+namespace Microsoft.Office.Interop.Excel
+{
+    public class ExcelHandler
+    {
+        public Application application { get; }
+        public Workbook workbook { get; set; }
+        public Worksheet active_Worksheet { get; set; }
+
+        public ExcelHandler()
+        {
+            Application _application = new Application();
+
+        }
+
+        public ExcelHandler(Workbook defaultWorkbook) : this()
+        {
+            this.workbook = defaultWorkbook;
+        }
+        public ExcelHandler(Workbook defaultWorkbook, Worksheet defaultWorksheet) : this()
+        {
+            this.workbook = defaultWorkbook;
+            this.active_Worksheet = defaultWorksheet;
+        }
+        public void CreateWorkbook()
+        {
+            this.application.Workbooks.Add("New workbook");
+        }
+
+
+        /*
+         * Microsoft Visual Studio 17.0 .NET Framework Version v4.6.1
+         * Automatically generated ..........
+         *                         Equals
+         *                         GetHasCode
+         *                         ==
+         *                         !=
+         */
+
+        public override bool Equals(object obj)
+        {
+            var handler = obj as ExcelHandler;
+            return handler != null &&
+                   EqualityComparer<Application>.Default.Equals(_application, handler._application) &&
+                   EqualityComparer<Workbook>.Default.Equals(workbook, handler.workbook) &&
+                   EqualityComparer<Worksheet>.Default.Equals(active_Worksheet, handler.active_Worksheet);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 168020149;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Application>.Default.GetHashCode(_application);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Workbook>.Default.GetHashCode(workbook);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Worksheet>.Default.GetHashCode(active_Worksheet);
+            return hashCode;
+        }
+
+        public static bool operator ==(ExcelHandler handler1, ExcelHandler handler2)
+        {
+            return EqualityComparer<ExcelHandler>.Default.Equals(handler1, handler2);
+        }
+
+        public static bool operator !=(ExcelHandler handler1, ExcelHandler handler2)
+        {
+            return !(handler1 == handler2);
+        }
+    }
+
+}
